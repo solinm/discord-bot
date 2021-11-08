4ac0e483b333bdb8d3f95c4b11d379372a01f8b8
Solin Maaroof <smaar008@uottawa.ca>
2021-11-08 10:07:55 +0300

    added message response functionality

--- on_message.py
+++ on_message.py
@@ -1,3 +1,17 @@
 import discord
 
-client = discord.Client
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("Bot is online!")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if message.content == "hello" or message.content == "hey" or message.content == "hi":
+        await message.channel.send("Hi!")
+
+client.run("OTA1OTI3MzMzNzA4NDQzNjU4.YYRMkw.hLmaACAG7SQhiPc_qoVVQk_IoNU")
